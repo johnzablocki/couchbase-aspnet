cf44ef8a87bc8292ebdcfbfdd131773e4676d544
jzablocki <dev@zblock.net>
2012-04-24 14:10:49 -0400

    Initial port of Enyim MemcachedClient SessionState provider to CouchbaseClient
    Nuget reference to CouchbaseClient 1.0.1 included.

---
+++ Couchbase.AspNet/Web/CouchbaseHttpApplication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Couchbase.AspNet.Web {
+    public class CouchbaseHttpApplication : HttpApplication {
+    }
+}
