ab2ae92a78bec09c8d032925774e2bd6a342461a
jzablocki <dev@zblock.net>
2012-04-24 14:12:25 -0400

    Initial import of CouchbaseAspNetSample project.
    This commit demonstrates usage of the SessionState provider.

---
+++ CouchbaseAspNetSample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CouchbaseAspNetSample.Constants;
+
+namespace CouchbaseAspNetSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Session[SessionConstants.MESSAGE] = "Hello, Couchbase ASP.NET!";
+
+			return View();
+        }
+
+		public ActionResult About()
+		{			
+
+			return View();
+		}
+
+	}
+}
