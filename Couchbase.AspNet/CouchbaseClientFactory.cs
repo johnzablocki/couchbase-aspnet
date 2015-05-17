57a0d2a06d06c648eeb85dc13eb5f5a7b6ae9f3f
Kendall Bennett <kendallb@amain.com>
2015-05-16 20:49:56 -0700

    Cleaned up CouchbaseClientFactory to only require the bucket name in the Web.config file

--- Couchbase.AspNet/CouchbaseClientFactory.cs
+++ Couchbase.AspNet/CouchbaseClientFactory.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.Configuration;
-using System.Linq;
-using Couchbase.Configuration.Client;
-using Couchbase.Configuration.Client.Providers;
 using Couchbase.Core;
 
 namespace Couchbase.AspNet
@@ -15,43 +9,14 @@ namespace Couchbase.AspNet
         public IBucket Create(string name, NameValueCollection config, out bool disposeClient)
         {
             // This client should be disposed of as it is not shared
-            disposeClient = false;
+            disposeClient = true;
 
-            // Get the section name from the configuration file. If not found, create a default Couchbase client which 
-            // will get the configuration information from the default Couchbase client section in the Web.config file
-            var sectionName = ProviderHelper.GetAndRemove(config, "section", false);
-	        if (String.IsNullOrEmpty(sectionName))
-				return this.CreateClient(null);
-
-            // If a custom section name is passed in, get the section information and use it to construct the Couchbase client
-            var section = ConfigurationManager.GetSection(sectionName);
-            if (section == null)
-				throw new InvalidOperationException("Invalid config section: " + sectionName);
-			return CreateClient(section);
+            // Get the bucket name to use from the configuration file
+            var bucketName = ProviderHelper.GetAndRemove(config, "bucket", false);
+	        if (string.IsNullOrEmpty(bucketName))
+                throw new ConfigurationErrorsException("CouchbaseClientFactory could not find the 'bucket' name.");
+            return ClusterHelper.GetBucket(bucketName);
         }
-
-		private IBucket CreateClient(object config)
-		{
-		    IBucket bucket;
-		    var section = config as CouchbaseClientSection;
-
-		    BucketElement bucketElement = null;
-		    foreach (var item in section.Buckets)
-		    {
-		        bucketElement = (BucketElement) item;
-                break;
-		    }
-		    if (section != null)
-		    {
-		        bucket = ClusterHelper.GetBucket(bucketElement.Name);
-		    }
-		    else
-		    {
-		        const string message = "CreateClient requires a CouchbaseConfigSection. Found a {0} instead.";
-		        throw new ConfigurationErrorsException(string.Format(message, config.GetType().Name));
-		    }
-		    return bucket;
-		}
     }
 }
 
--- CouchbaseAspNetSample/Global.asax.cs
+++ CouchbaseAspNetSample/Global.asax.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 using Couchbase;
 using Couchbase.AspNet.Web;
-using Couchbase.Configuration.Client;
 
 namespace CouchbaseAspNetSample
 {
