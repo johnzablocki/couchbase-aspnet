f33b265b0ed0ded79843c301d8dd37449f603cbc
jeffrymorris <jeffrymorris@gmail.com>
2018-02-21 18:15:02 -0800

    Remove whitespace mess

--- src/Couchbase.AspNet/Session/CouchbaseSessionStateProvider.cs
+++ src/Couchbase.AspNet/Session/CouchbaseSessionStateProvider.cs
@@ -63,32 +63,32 @@ namespace Couchbase.AspNet.Session
         #endregion
 
         /*
-      * Takes as input the HttpContext instance for the current request and the SessionID value for the current request. 
-      * Retrieves session values and information from the session data store and locks the session-item data at the data 
-      * store for the duration of the request. The GetItemExclusive method sets several output-parameter values that inform 
+      * Takes as input the HttpContext instance for the current request and the SessionID value for the current request.
+      * Retrieves session values and information from the session data store and locks the session-item data at the data
+      * store for the duration of the request. The GetItemExclusive method sets several output-parameter values that inform
       * the calling SessionStateModule about the state of the current session-state item in the data store.
-      *
-      * If no session item data is found at the data store, the GetItemExclusive method sets the locked output parameter to 
-      * false and returns null. This causes SessionStateModule to call the CreateNewStoreData method to create a new 
+
+      * If no session item data is found at the data store, the GetItemExclusive method sets the locked output parameter to
+      * false and returns null. This causes SessionStateModule to call the CreateNewStoreData method to create a new
       * SessionStateStoreData object for the request.
       *
-      * If session-item data is found at the data store but the data is locked, the GetItemExclusive method sets the locked 
-      * output parameter to true, sets the lockAge output parameter to the current date and time minus the date and time when 
-      * the item was locked, sets the lockId output parameter to the lock identifier retrieved from the data store, and returns 
-      * null. This causes SessionStateModule to call the GetItemExclusive method again after a half-second interval, to attempt 
-      * to retrieve the session-item information and obtain a lock on the data. If the value that the lockAge output parameter 
-      * is set to exceeds the ExecutionTimeout value, SessionStateModule calls the ReleaseItemExclusive method to clear the lock 
+      * If session-item data is found at the data store but the data is locked, the GetItemExclusive method sets the locked
+      * output parameter to true, sets the lockAge output parameter to the current date and time minus the date and time when
+      * the item was locked, sets the lockId output parameter to the lock identifier retrieved from the data store, and returns
+      * null. This causes SessionStateModule to call the GetItemExclusive method again after a half-second interval, to attempt
+      * to retrieve the session-item information and obtain a lock on the data. If the value that the lockAge output parameter
+      * is set to exceeds the ExecutionTimeout value, SessionStateModule calls the ReleaseItemExclusive method to clear the lock
       * on the session-item data and then call the GetItemExclusive method again.
-      * 
-      * The actionFlags parameter is used with sessions whose Cookieless property is true, when the regenerateExpiredSessionId 
-      * attribute is set to true. An actionFlags value set to InitializeItem (1) indicates that the entry in the session data 
-      * store is a new session that requires initialization. Uninitialized entries in the session data store are created by a 
-      * call to the CreateUninitializedItem method. If the item from the session data store is already initialized, the 
+      *
+      * The actionFlags parameter is used with sessions whose Cookieless property is true, when the regenerateExpiredSessionId
+      * attribute is set to true. An actionFlags value set to InitializeItem (1) indicates that the entry in the session data
+      * store is a new session that requires initialization. Uninitialized entries in the session data store are created by a
+      * call to the CreateUninitializedItem method. If the item from the session data store is already initialized, the
       * actionFlags parameter is set to zero.
-      * 
-      * If your provider supports cookieless sessions, set the actionFlags output parameter to the value returned from the session 
-      * data store for the current item. If the actionFlags parameter value for the requested session-store item equals the 
-      * InitializeItem enumeration value (1), the GetItemExclusive method should set the value in the data store to zero after 
+      *
+      * If your provider supports cookieless sessions, set the actionFlags output parameter to the value returned from the session
+      * data store for the current item. If the actionFlags parameter value for the requested session-store item equals the
+      * InitializeItem enumeration value (1), the GetItemExclusive method should set the value in the data store to zero after
       * setting the actionFlags out parameter.*/
         private SessionStateStoreData GetSessionStoreItem(bool lockRecord,
             HttpContext context,
@@ -106,10 +106,10 @@ namespace Couchbase.AspNet.Session
             _log.TraceFormat("GetSessionStoreItem called for item {0} with lockId {1}.", id, lockId);
 
             if (id == null) return null;
-            
+
             /*
-             * If no session item data is found at the data store, the GetItemExclusive method sets the locked output parameter to 
-             * false and returns null. This causes SessionStateModule to call the CreateNewStoreData method to create a new 
+             * If no session item data is found at the data store, the GetItemExclusive method sets the locked output parameter to
+             * false and returns null. This causes SessionStateModule to call the CreateNewStoreData method to create a new
              * SessionStateStoreData object for the request.
              */
             var sessionData = Bucket.Get<SessionStateItem>(id);
@@ -122,12 +122,12 @@ namespace Couchbase.AspNet.Session
             }
 
             /*
-             * * If session-item data is found at the data store but the data is locked, the GetItemExclusive method sets the locked 
-             * output parameter to true, sets the lockAge output parameter to the current date and time minus the date and time when 
-             * the item was locked, sets the lockId output parameter to the lock identifier retrieved from the data store, and returns 
-             * null. This causes SessionStateModule to call the GetItemExclusive method again after a half-second interval, to attempt 
-             * to retrieve the session-item information and obtain a lock on the data. If the value that the lockAge output parameter 
-             * is set to exceeds the ExecutionTimeout value, SessionStateModule calls the ReleaseItemExclusive method to clear the lock 
+             * * If session-item data is found at the data store but the data is locked, the GetItemExclusive method sets the locked
+             * output parameter to true, sets the lockAge output parameter to the current date and time minus the date and time when
+             * the item was locked, sets the lockId output parameter to the lock identifier retrieved from the data store, and returns
+             * null. This causes SessionStateModule to call the GetItemExclusive method again after a half-second interval, to attempt
+             * to retrieve the session-item information and obtain a lock on the data. If the value that the lockAge output parameter
+             * is set to exceeds the ExecutionTimeout value, SessionStateModule calls the ReleaseItemExclusive method to clear the lock
              * on the session-item data and then call the GetItemExclusive method again.
              */
 
@@ -191,10 +191,10 @@ namespace Couchbase.AspNet.Session
         }
 
         /*
-         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request, 
-         * and the lock identifier for the current request, and releases the lock on an item in the session data store. 
-         * This method is called when the GetItem or GetItemExclusive method is called and the data store specifies that 
-         * the requested item is locked, but the lock age has exceeded the ExecutionTimeout value. The lock is cleared by 
+         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request,
+         * and the lock identifier for the current request, and releases the lock on an item in the session data store.
+         * This method is called when the GetItem or GetItemExclusive method is called and the data store specifies that
+         * the requested item is locked, but the lock age has exceeded the ExecutionTimeout value. The lock is cleared by
          * this method, freeing the item for use by other requests.
          * */
         public override void ReleaseItemExclusive(HttpContext context, string id, object lockId)
@@ -221,15 +221,15 @@ namespace Couchbase.AspNet.Session
         }
 
         /*
-         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request, 
-         * a SessionStateStoreData object that contains the current session values to be stored, the lock identifier for 
-         * the current request, and a value that indicates whether the data to be stored is for a new session or an existing 
+         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request,
+         * a SessionStateStoreData object that contains the current session values to be stored, the lock identifier for
+         * the current request, and a value that indicates whether the data to be stored is for a new session or an existing
          * session.
-           If the newItem parameter is true, the SetAndReleaseItemExclusive method inserts a new item into the data store 
+           If the newItem parameter is true, the SetAndReleaseItemExclusive method inserts a new item into the data store
            with the supplied values. Otherwise, the existing item in the data store is updated with the supplied values, and
-           any lock on the data is released. Note that only session data for the current application that matches the supplied 
+           any lock on the data is released. Note that only session data for the current application that matches the supplied
            SessionID value and lock identifier values is updated.
-           After the SetAndReleaseItemExclusive method is called, the ResetItemTimeout method is called by SessionStateModule to 
+           After the SetAndReleaseItemExclusive method is called, the ResetItemTimeout method is called by SessionStateModule to
            update the expiration date and time of the session-item data.
          */
         public override void SetAndReleaseItemExclusive(HttpContext context, string id, SessionStateStoreData item, object lockId,
@@ -300,7 +300,7 @@ namespace Couchbase.AspNet.Session
             {
                 var reader = new BinaryReader(ms);
                 return SessionStateItemCollection.Deserialize(reader);
-            }  
+            }
         }
 
         public override void RemoveItem(HttpContext context, string id, object lockId, SessionStateStoreData item)
@@ -349,30 +349,30 @@ namespace Couchbase.AspNet.Session
             _log.Trace("CreateNewStoreData called.");
             return new SessionStateStoreData(new SessionStateItemCollection(),
                 SessionStateUtility.GetSessionStaticObjects(context),
-                timeout);   
+                timeout);
         }
 
         /*
-         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request, 
-         * and the lock identifier for the current request, and adds an uninitialized item to the session data store with 
+         * Takes as input the HttpContext instance for the current request, the SessionID value for the current request,
+         * and the lock identifier for the current request, and adds an uninitialized item to the session data store with
          * an actionFlags value of InitializeItem.
-         * 
-         * The CreateUninitializedItem method is used with cookieless sessions when the regenerateExpiredSessionId attribute 
+         *
+         * The CreateUninitializedItem method is used with cookieless sessions when the regenerateExpiredSessionId attribute
          * is set to true, which causes SessionStateModule to generate a new SessionID value when an expired session ID is encountered.
-         * 
-         * The process of generating a new SessionID value requires the browser to be redirected to a URL that contains the newly 
+         *
+         * The process of generating a new SessionID value requires the browser to be redirected to a URL that contains the newly
          * generated session ID. The CreateUninitializedItem method is called during an initial request that contains an expired
-         * session ID. After SessionStateModule acquires a new SessionID value to replace the expired session ID, it calls the 
-         * CreateUninitializedItem method to add an uninitialized entry to the session-state data store. The browser is then redirected 
-         * to the URL containing the newly generated SessionID value. The existence of the uninitialized entry in the session data 
-         * store ensures that the redirected request with the newly generated SessionID value is not mistaken for a request for an 
+         * session ID. After SessionStateModule acquires a new SessionID value to replace the expired session ID, it calls the
+         * CreateUninitializedItem method to add an uninitialized entry to the session-state data store. The browser is then redirected
+         * to the URL containing the newly generated SessionID value. The existence of the uninitialized entry in the session data
+         * store ensures that the redirected request with the newly generated SessionID value is not mistaken for a request for an
          * expired session, and instead is treated as a new session.
-         * 
-         * The uninitialized entry in the session data store is associated with the newly generated SessionID value and contains only 
-         * default values, including an expiration date and time, and a value that corresponds to the actionFlags parameter of the GetItem 
-         * and GetItemExclusive methods. The uninitialized entry in the session state store should include an actionFlags value equal to 
-         * the InitializeItem enumeration value (1). This value is passed to SessionStateModule by the GetItem and GetItemExclusive 
-         * methods and specifies for SessionStateModule that the current session is a new session. SessionStateModule will then initialize 
+         *
+         * The uninitialized entry in the session data store is associated with the newly generated SessionID value and contains only
+         * default values, including an expiration date and time, and a value that corresponds to the actionFlags parameter of the GetItem
+         * and GetItemExclusive methods. The uninitialized entry in the session state store should include an actionFlags value equal to
+         * the InitializeItem enumeration value (1). This value is passed to SessionStateModule by the GetItem and GetItemExclusive
+         * methods and specifies for SessionStateModule that the current session is a new session. SessionStateModule will then initialize
          * the new session and raise the Session_OnStart event.
          */
         public override void CreateUninitializedItem(HttpContext context, string id, int timeout)
@@ -460,21 +460,20 @@ namespace Couchbase.AspNet.Session
 
 #region [ License information          ]
 /* ************************************************************
- * 
  *    @author Couchbase <info@couchbase.com>
  *    @copyright 2017 Couchbase, Inc.
- *    
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *    
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- *    
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
- *    
+ *
  * ************************************************************/
 #endregion
