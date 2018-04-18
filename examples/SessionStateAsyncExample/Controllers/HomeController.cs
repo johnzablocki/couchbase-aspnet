10e722c00989137e4a23ed00fd91132159390a07
jeffrymorris <jeffrymorris@gmail.com>
2018-04-17 18:00:26 -0700

    #81: Cleanup directory structure of examples

---
+++ examples/SessionStateAsyncExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionStateAsyncExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Session["User"] = "Ted";
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page for " + Session["User"];
+
+            return View();
+        }
+    }
+}
---
+++ examples/SessionStateAsyncExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SessionStateAsyncExample.MvcApplication" Language="C#" %>
